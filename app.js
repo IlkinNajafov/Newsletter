a2d20c812bd25a31a5a1c137ce6ada8576b257fa
IlkinNajafov <58183512+IlkinNajafov@users.noreply.github.com>
2020-04-28 21:40:18 +0400

    dynamic city names and front-end page added

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require("express");
 const https = require("https");
-
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 
 app.get("/", function (req, res) {
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Baku,az&appid=5b60fa1db06d9a924739377a4a09d3c9&units=metric"
-      
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+    const query = req.body.cityName;
+    const appid = "5b60fa1db06d9a924739377a4a09d3c9";
+    const unit = "metric";
+
+    const url = "https://api.openweathermap.org/data/2.5/weather?q= " + query + "&appid=" + appid + "&units=" + unit + ""
+
     https.get(url, function (response) {
         console.log(response.statusCode);
 
@@ -19,7 +30,7 @@ app.get("/", function (req, res) {
             const icon = weatherData.weather[0].icon
             const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
             res.write("<p>The weather is currently " + weatherDescription + " </p>");
-            res.write("<h1>The temperature is " + temp + " degrees in Baku</h1>");
+            res.write("<h1>The temperature shows " + temp + " degree(s) in " + query + "</h1>");
             res.write("<img src=" + imageURL + ">");
             res.send();
         });
@@ -32,7 +43,6 @@ app.get("/", function (req, res) {
 
 
 
-
 app.listen(3000, function () {
-    console.log("Server is running on port 3000");    
+    console.log("Server is running on port 3000");
 });
